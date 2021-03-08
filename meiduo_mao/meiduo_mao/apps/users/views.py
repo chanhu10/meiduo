827a8ed557de2e076b624f8d929f8955c474779f
chanhu10 <chanhu10@126.com>
2021-03-08 23:56:50 +0800

    用户注册后端逻辑

--- meiduo_mao/meiduo_mao/apps/users/views.py
+++ meiduo_mao/meiduo_mao/apps/users/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
 from django.views import View
+
+from django import http
+import re
+
+from users.models import User
+from django.db import DatabaseError
 # Create your views here.
 
 
@@ -12,5 +18,38 @@ class Register(View):
 
         return render(request, 'register.html')
 
-    def post(self, requuest):
-        pass
+    def post(self, request):
+
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        password2 = request.POST.get("password2")
+        mobile = request.POST.get("mobile")
+        allow = request.POST.get("allow")
+
+        if not all([username, password, password2, mobile, allow]):
+            return http.HttpResponseForbidden("缺少必要参数")
+
+        if not re.match(r'^[a-z0-9A-Z_-]{5,20}$', username):
+            return http.HttpResponseForbidden("请输入5-20个字符的用户名")
+
+        if not re.match(r'^[0-1a-zA-Z]{8,20}', password):
+            return http.HttpResponseForbidden("请输入8-20位的密码")
+
+        if not password == password2:
+            return http.HttpResponseForbidden("2次输入的密码不一致")
+
+        if not re.match(r'^1[3-9]\d{9}$]', mobile):
+            return http.HttpResponseForbidden("请输入正确的手机号")
+
+        if allow != "on":
+            return http.HttpResponseForbidden("请勾选用户协议")
+
+        try:
+            User.objects.create_user(username=username, password=password, mobile=mobile)
+        except DatabaseError:
+            render(request, 'register.html', {"register_errmsg": "注册失败"})
+
+        return http.HttpResponse("注册成功，重定向到首页")
+
+
+
