f1efd48500189aa25260ebada7e31eec1952a8ca
chanhu10 <chanhu10@126.com>
2021-04-08 23:48:12 +0800

    完成首页商品分类

--- meiduo_mao/meiduo_mao/apps/contents/views.py
+++ meiduo_mao/meiduo_mao/apps/contents/views.py
@@ -1,11 +1,41 @@
 from django.shortcuts import render
 
 from django.views import View
+
+
+from goods.models import GoodsChannelGroup, GoodsChannel, GoodsCategory
 # Create your views here.
 
 class IndexView(View):
 
     def get(self, request):
 
-        return render(request, 'index.html')
+        categories = {}
+        channels = GoodsChannel.objects.all()
+        for channel in channels:
+            group_id = channel.group_id
+            if group_id not in categories.keys():
+                categories[group_id] = {"channels": [], "sub_cats":[]}
+
+            cat1 = channel.category
+
+            categories[group_id]["channels"].append({
+                "id": cat1.id,
+                "name": cat1.name,
+                "url": channel.url
+            })
+
+            for cat2 in cat1.subs.all():
+                cat2.sub_cats = []
+                for cat3 in cat2.subs.all():
+                    cat2.sub_cats.append(cat3)
+
+                categories[group_id]["sub_cats"].append(cat2)
+
+        context = {
+            "categories":categories
+        }
+
+        return render(request, 'index.html', context)
+
 
